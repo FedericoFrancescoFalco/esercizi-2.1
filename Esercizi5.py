9b2fc26653cf6b8f1ffc803aa1271cde5e98917d
FedericoFrancescoFalco <gidanrazorblade@gmail.com>
2024-05-07 13:22:09 +0200

    commit

---
+++ Esercizi5.py
@@ -0,0 +1,6 @@
+# Create a Playlist:Write a function called create_playlist() that accepts a playlist name and a variable number of song titles
+#The function should return a dictionary with the playlist name and a set of songs. Call the function with different numbers of
+#songs to demonstrate flexibility.
+#Example: create_playlist("Road Trip", {"Song 1", "Song 2"})
+#Write a function called add_like() that accepts a dictionary, the name of a playlist, and a boolean value indicating whether it is liked (True or False). This function should return an updated dictionary.
+#Example: add_like(dictionary, “Road Trip”, liked=True)
