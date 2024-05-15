eed0aa3629957c3f50c66e7b2df5cc0111fd0ada
FedericoFrancescoFalco <gidanrazorblade@gmail.com>
2024-05-15 13:04:37 +0200

    ripasso

--- TurtleGame.py
+++ TurtleGame.py
@@ -1,50 +1,74 @@
 
+#uso un modulo per generare numeri random
 import random
-posizionetarta = 20
-posizionelepre = 40
-indicetarta = posizionetarta -1
-indicelepre = posizionelepre -1
-lunghezza_lista = 70
-percorso: list[str] = ["_"] * lunghezza_lista
 
-def posizione(indicelepre:int , indicetarta:int, percorso:int ):
-    for i in percorso:
-        if i == indicelepre:
-            percorso.remove("_")
-            percorso.append("H")
-        elif i == indicetarta:
-            percorso.remove("_")
-            percorso.append("T")
+#definisco una funzione con tre argomenti: la posizione generale, quella della tararuga, quella della lepre. in un ciclo da 70 istanti in cui il ciclo for ha tre condizioni per risolversi oppure.
 
-        print(percorso)
-
-def mossatarta(posizionetarta):
-    numero = random.randint(1,10)
-    if 1 <= numero <= 5:
-        posizionetarta += 3
-
-    elif 6 <= numero <= 7:
-        if posizionetarta >= 7:
-            posizionetarta -= 6
-
+def print_race(positions, hare_pos, tortoise_pos):
+    for i in range(70):
+        if i == hare_pos and i == tortoise_pos:
+            print("OUCH!!!", end="")
+        elif i == hare_pos:
+            print("H", end="")
+        elif i == tortoise_pos:
+            print("T", end="")
         else:
-            posizionetarta =1 
+            print("_", end="")
+    print()
 
-    elif 8<= numero <= 10:
-        posizionetarta +=1
+#Le funzioni `tortoise_move()` e `hare_move()` determinano quanto la tartaruga e la lepre si spostano durante ogni turno della gara. All'interno di queste funzioni, viene generato un numero casuale tra 1 e 10. Questo numero rappresenta quanto velocemente la tartaruga o la lepre si muoverà.
 
+def tortoise_move():
+    move = random.randint(1, 10)
+    if move <= 5:
+        return 3
+    elif 6 <= move <= 7:
+        return -6
+    else:
+        return 1
 
-def mossalepre(posizione lepre, indicelepre):
-    numero = random.randint(1,10)
-    if 1 <= numero <= 2:
-        posizionetarta *= 0
-    
-    elif 3 <= numero <= 4:
-        posizionelepre += 9
+def hare_move():
+    move = random.randint(1, 10)
+    if move <= 2:
+        return 0
+    elif 3 <= move <= 4:
+        return 9
+    elif 5 <= move <= 6:
+        return -12
+    elif 7 <= move <= 9:
+        return 1
+    else:
+        return -2
 
+#definisco una posizione iniziale tra la tartaruga e la lepre. utilizzo il ciclo il ciclo while true che prodeguirà finché non  interrotto dall' istruzione `break`. Le posizioni della lepre e della tartaruga vengono aggiornate ad ogni iterazione utilizzando le funzioni `hare_move()` e `tortoise_move()` , generate da un numero casuale. Con relativa verifica che le posizioni non siano mai inferiori a 1.
 
+def race():
+    hare_pos = 1
+    tortoise_pos = 1
+    print("BANG !!!!! AND THEY'RE OFF !!!!!")
+    while True:
+        hare_pos += hare_move()
+        tortoise_pos += tortoise_move()
+        
+        if hare_pos < 1:
+            hare_pos = 1
+        if tortoise_pos < 1:
+            tortoise_pos = 1
+   
+    #uso la funzione `print_race` per stampare la visualizzazione della gara. E una serie di verifiche per capire chi stia vincendo.        
+                              
+        print_race(70, hare_pos, tortoise_pos)
+        
+        if hare_pos >= 70 and tortoise_pos >= 70:
+            print("IT'S A TIE.")
+            break
+        elif hare_pos >= 70:
+            print("HARE WINS || YUCH!!!")
+            break
+        elif tortoise_pos >= 70:
+            print("TORTOISE WINS! || VAY!!!")
+            break
 
+race()
 
-
-
-# https://github.com/FedericoFrancescoFalco/esercizi-2.1.git
+#quando una delle condizioni di vittoria viene soddisfatto,il programma termina.
---
+++ ripasso.py
@@ -0,0 +1,109 @@
+nome_variabile: int = 10
+nome_variabile: float = 10
+nome_variabile: bool = False
+nome_variabile: str = "Ciao"
+
+nome_variabile_ int =10
+
+nome_variabile = nome_variabile + 5
+nome_variabile += 5.5
+
+variabile_float: float = 0.0
+
+variabile_float = 5.0 + 10.0
+
+variabile_int: int = 0
+variabile_int = 10 + 5
+
+print(nome_variabile)
+
+
+lunghezza_lista: int = 7
+punto_di_mezzo: int = 7 // 2
+
+import math
+punto_di_mezzo: float = 3.2
+print(math.ceil(punto_di_mezzo))
+
+var_1: bool = True
+var_2: bool = False
+
+
+print(var_1 and var_2)
+print(var_1 or var_2)
+print(not var_1)
+
+
+if var_1 and var_2:
+
+    print(f"{var_1 and var_2}")
+
+x: int = 1000
+
+if x > 0 and x < 20:
+
+    if lista[i] > lista[i+1]:
+        
+        temp: int = lista[i]
+
+        lista[i] = lista[i+1]
+
+        lista[i+1] = temp
+
+    elif var_1 and var_2
+
+    else:
+
+
+
+for x in range(10):
+    print(f"x = {x}")
+
+
+
+
+a: bool = True
+a: bool = True
+
+
+if a and b:
+
+    print(f"sono nel primo if")
+
+
+elif a or b:
+
+    print(f"sono nel primo elif")
+
+else:
+
+    print(f"Sono nell'else")
+
+
+    #tre modi di fare la stessa cosa
+
+
+lista: list =[1, 2, 3, 5]
+
+for numero in lista:
+    print(f"xì2: {numero**2}")
+
+    prima stampa 1, poi 4, poi 9, poi 25
+
+
+nel caso di
+for numero in range(len(lista)):
+    print(f"x^2: {list[numero]**2}")
+
+ 
+
+
+contatore: int = 0
+while contatore < len(lista):
+
+    print(f"x^2: {lista[contatore]**2}")
+
+    contatore +=1
+
+
+
